61db37e81e0f8f88617074c80ea11008f3f581b5
jbaptiste51 <jean.noiraud@gmail.com>
2023-07-19 15:18:05 +0200

    first commit with sass and db

---
+++ backend/src/controllers/kanjiControllers.js
@@ -0,0 +1,91 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.kanji
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.kanji
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const kanji = req.body;
+
+  // TODO validations (length, format...)
+
+  kanji.id = parseInt(req.params.id, 10);
+
+  models.kanji
+    .update(kanji)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const kanji = req.body;
+
+  // TODO validations (length, format...)
+
+  models.kanji
+    .insert(kanji)
+    .then(([result]) => {
+      res.location(`/kanji/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.kanji
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
