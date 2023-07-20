12413d54d3655c991a12ecb0c38ef9606117248a
jbaptiste51 <jean.noiraud@gmail.com>
2023-07-20 12:12:25 +0200

    link, scss and home picture added

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,8 +1,27 @@
+import { Link } from "react-router-dom";
+import "../assets/font/Fondamento-Regular.ttf";
+
 export default function Header() {
   return (
     <header className="header">
       <div className="logo">
-        <p>KANJITEN</p>
+        <Link to="../" className="homelink">
+          <p>KANJITEN</p>
+        </Link>
+      </div>
+      <div className="link-div">
+        <Link to="../kanji" className="hlink">
+          <p>KANJI</p>
+        </Link>
+        <Link to="../hiragana" className="hlink">
+          <p>HIRAGANA</p>
+        </Link>
+        <Link to="../katakana" className="hlink">
+          <p>KATAKANA</p>
+        </Link>
+        <Link to="../signin" className="hlink">
+          <p>CONNEXION</p>
+        </Link>
       </div>
     </header>
   );
