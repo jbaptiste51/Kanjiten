2a194a48440d5cfdf6c5e8c026bda34be281d255
jbaptiste51 <jean.noiraud@gmail.com>
2023-07-20 16:59:24 +0200

    contact page scss

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,7 +1,47 @@
+import contactimage from "../assets/image/contact.jpg";
+
 export default function Contact() {
   return (
-    <div>
-      <p>contact</p>
+    <div className="contact">
+      <div className="contactbox">
+        <div className="form-label">
+          <label htmlFor="email">
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Adresse Mèl"
+              className="emailbox"
+            />
+          </label>
+        </div>
+        <div className="form-label">
+          <label htmlFor="object">
+            <input type="text" placeholder="Objet" className="objectbox" />
+          </label>
+        </div>
+        <div className="message">
+          <label htmlFor="message">
+            <textarea
+              id="message"
+              name="message"
+              rows="16"
+              maxLength="250"
+              cols="60"
+              wrap
+              placeholder="Message"
+              className="messagebox"
+            />
+          </label>
+        </div>
+        <button type="submit" className="contact-button">
+          Envoyer
+        </button>
+      </div>
+
+      <div className="contact-image">
+        <img src={contactimage} alt="Contact" className="contact-image" />
+      </div>
     </div>
   );
 }
