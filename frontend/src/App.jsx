dedf321b0b343f90364fdd188558c17b59b5f498
jbaptiste51 <jean.noiraud@gmail.com>
2023-07-20 10:48:28 +0200

    import added in app jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,14 @@ import "./assets/font/Fondamento-Regular.ttf";
 import background from "./assets/image/background.jpg";
 
 import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Contact from "./pages/Contact";
+import Kanji from "./pages/Kanji";
+import Katakana from "./pages/Katakana";
+import Hiragana from "./pages/Hiragana";
 import Header from "./components/Header";
 
 import "./App.css";
@@ -26,6 +34,14 @@ function App() {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/kanji" element={<Kanji />} />
+            <Route path="/hiragana" element={<Hiragana />} />
+            <Route path="/katakana" element={<Katakana />} />
           </Routes>
         </BrowserRouter>
       </div>
