8099c12fe368a20c8cf61bf6504a778f14ccab3a
jbaptiste51 <jean.noiraud@gmail.com>
2023-07-20 15:14:11 +0200

    ajust some scss

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,10 +4,14 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div>
-        <Link to="../about" className="about-link">A PROPOS</Link>
+        <Link to="../about" className="about-link">
+          A PROPOS
+        </Link>
       </div>
       <div>
-        <Link to="../contact" className="contact-link">CONTACT</Link>
+        <Link to="../contact" className="contact-link">
+          CONTACT
+        </Link>
       </div>
     </footer>
   );
