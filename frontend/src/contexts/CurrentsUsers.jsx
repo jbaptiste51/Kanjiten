f829223618388ccb7e760bb3c2927a113fc67760
jbaptiste51 <jean.noiraud@gmail.com>
2023-07-21 09:56:02 +0200

    controllers and managers for hiragana and katakana added

--- frontend/src/contexts/CurrentsUsers.jsx
+++ frontend/src/contexts/CurrentsUsers.jsx
@@ -4,9 +4,11 @@ const CurrentUserContext = createContext();
 
 export default CurrentUserContext;
 
+// eslint-disable-next-line react/prop-types
 export function CurrentUserProvider({ children }) {
   const [currentUser, setCurrentUser] = useState({});
   return (
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
     <CurrentUserContext.Provider value={{ currentUser, setCurrentUser }}>
       {children}
     </CurrentUserContext.Provider>
